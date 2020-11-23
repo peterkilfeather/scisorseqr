83cb170cf3fbfd62c76b6710d98dea855364926b
peterkilfeather <pkilfeather1@gmail.com>
2020-11-23 15:48:08 +0000

    Update f2t.sh

--- inst/bash/f2t.sh
+++ inst/bash/f2t.sh
@@ -2,10 +2,11 @@
 
 #if line starts with ">" 
 #if not on the first row of the file, 
-#use the newline separator to delimit
+#print a newline then
 #print the name of the fasta sequence without ">"
-#if on line 1, do the same
-#otherwise if does not start with ">" print whole line (FASTA sequence)
+#if on line 1, do as above except dont print a newline
+#otherwise if does not start with ">" print whole line (FASTA sequence) 
+#on the same line as the chr number
 #NB. outputs fasta sequence all on one line
 function FastaToTbl()
 {
